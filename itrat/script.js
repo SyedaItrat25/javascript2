45e16befc035af047bd70ee5f040d70686c10c28
Syeda Itrat Absar <syedaitrutabsar@gmail.com>
2024-05-13 15:49:21 +0500

    javascript2

---
+++ itrat/script.js
@@ -0,0 +1,24 @@
+// let number1 = +prompt ("Enter your number");
+// let number2 = +prompt ("Enter your number");
+// console.log(number1 + number2);
+
+// let y = 12;
+// let z = 18;
+// console.log(y!==z);
+// let mode = "light";
+// let color;
+// if(mode == "dark") {
+//     color = "black";
+
+// console.log(color);
+// }
+// else{
+//     console.log("white");
+// }
+let age = +prompt("Enter your age");
+if (age > 18) {
+    console.log("You are eligible for voting");
+} 
+else{
+    console.log("You are not eligible for voting");
+}
